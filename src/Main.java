e6bd7c50a5560e7642a6bc30b490e87af20b6c7f
SimonSaraMaria <sara.simon@stud.ubbcluj.ro>
2025-02-18 12:05:05 +0200

    1 c

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,21 @@ public class Main {
     public static void hoherenglobalenwert(List<MarvelLog> marvellog, Integer wert) {
         marvellog.stream()
                 .filter(marvelLog -> marvelLog.getGlobalerEinfluss()>= wert)
-                .forEach(marvelLog -> System.out.println(marvelLog.getHeld());
+                .forEach(marvelLog -> System.out.println(marvelLog.getHeld()));
 
     }
+
+    public static void galakkonfrontationen(List<MarvelLog> marvellog, Konfrontation konfrontation)
+    {
+        System.out.println("Tasks of the Space group sorted by difficulty from tsv file:");
+        marvellog.stream()
+                .filter(log -> log.getKonfrontation() == Konfrontation.GALAKTISCH)
+                .sorted(Comparator.comparing(MarvelLog::getKonfrontation))
+                .map(MarvelLog::getKonfrontation)
+                .forEach(System.out::println);
+
+        System.out.println();
+    }
 }
+
+
